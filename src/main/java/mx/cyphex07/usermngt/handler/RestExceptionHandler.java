0f15d6223a78fb553484d9978d63a714d1e3ac54
hector-silvar <hector.silvar@mediamonks.com>
2022-07-27 18:50:57 -0500

    Mapping UserAlreadyExistException into exception handler

--- src/main/java/mx/cyphex07/usermngt/handler/RestExceptionHandler.java
+++ src/main/java/mx/cyphex07/usermngt/handler/RestExceptionHandler.java
@@ -3,6 +3,7 @@ package mx.cyphex07.usermngt.handler;
 import mx.cyphex07.usermngt.exception.ApiError;
 import mx.cyphex07.usermngt.exception.BadRequestException;
 import mx.cyphex07.usermngt.exception.ResourceNotFoundException;
+import mx.cyphex07.usermngt.exception.UserAlreadyExistException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +18,19 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
   @ExceptionHandler(value = {BadRequestException.class})
   public ResponseEntity<ApiError> resourceNotFoundException(BadRequestException ex, WebRequest request) {
     final ApiError apiError = message(HttpStatus.BAD_REQUEST, ex);
-
     return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(value = {ResourceNotFoundException.class})
   public ResponseEntity<ApiError> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
     final ApiError apiError = message(HttpStatus.NOT_FOUND, ex);
-
     return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(value = {UserAlreadyExistException.class})
+  public ResponseEntity<ApiError> userAlreadyExistException(UserAlreadyExistException ex, WebRequest request) {
+    final ApiError apiError = message(HttpStatus.BAD_REQUEST, ex);
+    return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
   }
 
   protected ApiError message(final HttpStatus httpStatus, final Exception ex) {
